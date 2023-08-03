e3bac8fe5c5a008f83c36d422fa8f1776a55a27b
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-08-03 10:58:33 +0100

    Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+import path from "path";
+import fs from "fs/promises"
+import Link from "next/link";
+const HomePage = (props) => {
+  const {products} = props
+  return <ul>
+    {products.map(item => <li key={item.id}><Link href={`/products/${item.id}`}>{item.title}</Link></li>)}
+  </ul>
+}
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy_backend.json");
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
+  if (!data) {
+    return {
+      redirect : {
+        destination: "/no-path"
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10
+  }
 }
 
 export default HomePage;
